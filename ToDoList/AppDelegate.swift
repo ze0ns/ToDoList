b1f7c2774f1104c7dd54b551ddc880927c72e1b7
ze0ns <80447042+ze0ns@users.noreply.github.com>
2021-04-17 12:35:35 +0300

    Добавлены иконки оповещения к ярлыку на рабочем столе (количество не выполненных дел)

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        requestForNotification()
+        setBadge()
         // Override point for customization after application launch.
         return true
     }
--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import UserNotifications
+import UIKit
+
 var ToDoItems: [[String: Any]] {
     set
     {  print("Сохраняем данные")
@@ -26,15 +29,18 @@ var ToDoItems: [[String: Any]] {
 func addItem(nameItem: String, isCompleted: Bool = false) {
     ToDoItems.append(["Name":nameItem,"isCompleted": isCompleted])
     saveData()
+    setBadge()
 }
     
 func removeItem(at index: Int){
     ToDoItems.remove(at: index)
+    setBadge()
     saveData()
 }
 func changeState(at item: Int) -> Bool{
     ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
     saveData()
+    setBadge()
     return (ToDoItems[item]["isCompleted"] != nil)
     
 }
@@ -42,6 +48,29 @@ func moveItem(fromIndex: Int, toIndex: Int){
     let from = ToDoItems[fromIndex]
     ToDoItems.remove(at: fromIndex)
     ToDoItems.insert(from, at: toIndex)
+}
+
+
+func requestForNotification(){
+    UNUserNotificationCenter.current().requestAuthorization(options: [.badge]) { (isEnabled, error) in
+        if isEnabled {
+            print("Согласие получено")
+        } else {
+            print("Отказ")
+        }
+        
+        
+    }
+}
+func setBadge(){
+    var totalBadgeNumber = 0
+    for item in ToDoItems {
+        if (item["isCompleted"] as? Bool)  == false {
+            totalBadgeNumber = totalBadgeNumber + 1
+        }
+    }
+    UIApplication.shared.applicationIconBadgeNumber = totalBadgeNumber
+    print(totalBadgeNumber)
 }
 
 func saveData(){
